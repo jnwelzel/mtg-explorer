e1754865e91924fa49b1e2371f7c0e6b6a93afee
Jonathan Welzel <jnwelzel@gmail.com>
2025-07-28 22:40:42 +0200

    Bigger click area set item

--- src/components/ui/SetItem.tsx
+++ src/components/ui/SetItem.tsx
@@ -3,8 +3,10 @@ import type { Set } from 'scryfall-api'
 
 export const SetItem: React.FC<{ set: Set }> = ({ set }) => {
   return (
-    <li className="flex items-center border rounded border-stone-400 px-2 md:p-3 py-2 shadow hover:shadow-md transition-shadow duration-200">
-      <NavLink to={`/sets/${set.code}`} className="flex items-center text-black hover:underline">
+    <li className="flex items-center border rounded border-stone-400 shadow hover:shadow-md transition-shadow duration-200">
+      <NavLink
+        to={`/sets/${set.code}`}
+        className="flex items-center text-black hover:underline px-2 md:p-3 py-2 w-full">
         <img src={set.icon_svg_uri} alt={`${set.name} icon`} className="w-5 h-5 mr-1" />
         {set.name}
       </NavLink>
