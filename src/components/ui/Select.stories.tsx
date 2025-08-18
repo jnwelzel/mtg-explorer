a90d11b6841d1ff6744e82b9169dd74ee699366e
Jonathan Welzel <jnwelzel@gmail.com>
2025-08-18 16:43:45 +0200

    Stories for Select

---
+++ src/components/ui/Select.stories.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { Select } from './Select'
+import { FaMagic } from 'react-icons/fa'
+
+export default {
+  title: 'Components/Select',
+  component: Select,
+}
+
+const options = [
+  { value: 'usd', label: 'USD' },
+  { value: 'eur', label: 'EUR' },
+  { value: 'tix', label: 'TIX' },
+]
+
+export const Default = () => {
+  const [value, setValue] = useState('usd')
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-6">
+      <Select options={options} value={value} onChange={setValue} name="currency" />
+    </div>
+  )
+}
+
+export const WithIcon = () => {
+  const [value, setValue] = useState('usd')
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-6">
+      <Select
+        options={options}
+        value={value}
+        onChange={setValue}
+        name="currency"
+        icon={<FaMagic />}
+      />
+    </div>
+  )
+}
